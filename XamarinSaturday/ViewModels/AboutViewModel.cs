08143d8185de6b3a00efd3c4cacaa35426df08e5
Dylan Berry <dberry@burstingsilver.com>
2019-08-25 17:23:04 -0400

    Initial spike

---
+++ XamarinSaturday/ViewModels/AboutViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace XamarinSaturday.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        private string longitude;
+        public string Longitude
+        {
+            get { return longitude; }
+            set { SetProperty(ref longitude, value); }
+        }
+
+        private string latitude;
+        public string Latitude
+        {
+            get { return latitude; }
+            set { SetProperty(ref latitude, value); }
+        }
+
+        public AboutViewModel()
+        {
+            Title = "About";
+
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+
+            GetLocation = new Command(async () =>
+            {
+                try
+                {
+                    var result = await Xamarin.Essentials.Geolocation.GetLocationAsync();
+                    Latitude = result.Latitude.ToString();
+                    Longitude = result.Longitude.ToString();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            });
+        }
+
+        public ICommand OpenWebCommand { get; }
+
+        public ICommand GetLocation { get; }
+    }
+}
---
+++ XamarinSaturday/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinSaturday.Models;
+
+namespace XamarinSaturday.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
